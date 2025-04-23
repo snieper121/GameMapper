0ff2959bb0310e7827c80a2df6a7ee4a03604df3
snieper121 <mredozubovlist@gmail.com>
2025-04-23 22:32:21 +0300

    Add files via upload

--- app/src/main/java/com/example/gamemapper/di/AppModule.kt
+++ app/src/main/java/com/example/gamemapper/di/AppModule.kt
@@ -4,44 +4,56 @@ import android.content.Context
 import com.example.gamemapper.repository.interfaces.IMappingRepository
 import com.example.gamemapper.repository.interfaces.IProfileRepository
 import com.example.gamemapper.repository.interfaces.ISettingsRepository
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * Модуль для предоставления зависимостей
  */
 object AppModule {
     private lateinit var appContainer: AppContainer
-
+    private val initialized = AtomicBoolean(false)
+    
+    @Synchronized
     fun initialize(context: Context) {
+        if (initialized.get()) return
         appContainer = AppContainer(context.applicationContext)
+        initialized.set(true)
+    }
+    
+    private fun checkInitialization() {
+        check(initialized.get()) { "AppModule должен быть инициализирован перед использованием" }
     }
 
     fun getProfileRepository(): IProfileRepository {
+        checkInitialization()
         return appContainer.profileRepository
     }
 
     fun getMappingRepository(): IMappingRepository {
+        checkInitialization()
         return appContainer.mappingRepository
     }
 
     fun getSettingsRepository(): ISettingsRepository {
+        checkInitialization()
         return appContainer.settingsRepository
     }
 
-    fun getErrorHandler() = appContainer.errorHandler
+    fun getErrorHandler() = appContainer.errorHandler.also { checkInitialization() }
 
-    fun getPermissionHelper() = appContainer.permissionHelper
+    fun getPermissionHelper() = appContainer.permissionHelper.also { checkInitialization() }
 
-    fun getFeedbackHelper() = appContainer.feedbackHelper
+    fun getFeedbackHelper() = appContainer.feedbackHelper.also { checkInitialization() }
 
-    fun getExternalDeviceDetector() = appContainer.externalDeviceDetector
+    fun getExternalDeviceDetector() = appContainer.externalDeviceDetector.also { checkInitialization() }
 
-    fun getNotificationHelper() = appContainer.notificationHelper
+    fun getNotificationHelper() = appContainer.notificationHelper.also { checkInitialization() }
 
-    fun getOverlayManager() = appContainer.overlayManager
+    fun getOverlayManager() = appContainer.overlayManager.also { checkInitialization() }
 
-    fun getMemoryOptimizedOverlayManager() = appContainer.memoryOptimizedOverlayManager
+    fun getMemoryOptimizedOverlayManager() = appContainer.memoryOptimizedOverlayManager.also { checkInitialization() }
 
-    fun getInputEventHandler() = appContainer.inputEventHandler
+    fun getInputEventHandler() = appContainer.inputEventHandler.also { checkInitialization() }
 
-    fun getProfileViewModelFactory() = appContainer.profileViewModelFactory
+    fun getProfileViewModelFactory() = appContainer.profileViewModelFactory.also { checkInitialization() }
 }
